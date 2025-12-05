deaae53c52f3e9b6b346261e7fe204ef79000e2b
Tyler Carbone <tcarbone073@gmail.com>
2025-12-05 10:41:03 -0500

    [main] Add USAGE, basic command-line parsing

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,53 @@
 
 #include "Tokenizer.h"
 
+void usage()
+{
+    std::cout << "OVERVIEW: A tool to format legacy (< Vim9) vimscript code."
+
+              << "\n\nUSAGE: vim-format [options] <file>"
+
+              << "\n\nREQUIRED ARGUMENTS:\n\n"
+
+              << "  <file>           File to format. '-' means read from stdin."
+
+              << "\n\nOPTIONS:\n\n"
+
+              << "  -h, --help       Display this help." << std::endl;
+}
+
 int main(int argc, char** argv)
 {
-    char c;
-    std::string lsText;
-    while (std::cin.get(c))
+    if (argc == 1)
     {
-        lsText += c;
+        std::cerr << "Error: missing <file>" << std::endl;
+        return 1;
+    }
+
+    std::string lsText;
+
+    for (int i = 1; i < argc; i++)
+    {
+        std::string lsArg = argv[i];
+
+        if ((lsArg == "-h") || (lsArg == "--help"))
+        {
+            usage();
+            return 0;
+        }
+        else if (lsArg == "-")
+        {
+            char c;
+            while (std::cin.get(c))
+            {
+                lsText += c;
+            }
+        }
+        else
+        {
+            std::cerr << "Error: unrecognized argument: " << lsArg << std::endl;
+            return 1;
+        }
     }
 
     Tokenizer lcTokenizer(lsText);
