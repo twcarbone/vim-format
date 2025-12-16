3a6b27e42e92a739f93808b5f2326b82be95ef71
Tyler Carbone <tcarbone073@gmail.com>
2025-12-16 14:55:57 -0500

    [Lex] Fix Tokenizer tests

--- test/Tokenizer_tests.cpp
+++ test/Tokenizer_tests.cpp
@@ -58,11 +58,20 @@ protected:
     }
 };
 
+//
+// SingleTokenTest
+//
+
 TEST_F(SingleTokenTest, IntegerLiteral)
 {
+    do_test("4", Token::Type::INTEGER);
     do_test("42", Token::Type::INTEGER);
+    do_test("423", Token::Type::INTEGER);
+    do_test("004", Token::Type::INTEGER);
     do_test("0x42", Token::Type::INTEGER);
     do_test("0X42", Token::Type::INTEGER);
+    do_test("0x42ff", Token::Type::INTEGER);
+    do_test("0Xa42f", Token::Type::INTEGER);
     do_test("0o42", Token::Type::INTEGER);
     do_test("0O42", Token::Type::INTEGER);
     do_test("0b1101", Token::Type::INTEGER);
@@ -71,10 +80,15 @@ TEST_F(SingleTokenTest, IntegerLiteral)
 
 TEST_F(SingleTokenTest, FloatLiteral)
 {
-    do_test("123.456", Token::Type::FLOAT);
+    do_test("1.0", Token::Type::FLOAT);
     do_test("55.0", Token::Type::FLOAT);
+    do_test("123.456", Token::Type::FLOAT);
     do_test("1.234e03", Token::Type::FLOAT);
-    do_test("1.0E-6", Token::Type::FLOAT);
+    do_test("1.234e-03", Token::Type::FLOAT);
+    do_test("1.234e+03", Token::Type::FLOAT);
+    do_test("1.234E03", Token::Type::FLOAT);
+    do_test("1.234E-03", Token::Type::FLOAT);
+    do_test("1.234E+03", Token::Type::FLOAT);
 }
 
 TEST_F(SingleTokenTest, WhiteSpace)
@@ -84,13 +98,70 @@ TEST_F(SingleTokenTest, WhiteSpace)
 
 TEST_F(SingleTokenTest, StringLiteral)
 {
-    do_test("'abcd'", Token::Type::STRING);
     do_test("\"abcd\"", Token::Type::STRING);
+}
+
+//
+// TokenizerTest
+//
+
+TEST_F(TokenizerTest, FloatTokenTest)
+{
+    tokenize_str("2.2.2");
+    expect_tokens(4);
+    expect_token(0, "2", Token::Type::INTEGER);
+    expect_token(1, ".", Token::Type::OP_CAT_OLD);
+    expect_token(2, "2.2", Token::Type::FLOAT);
+
+    tokenize_str("3.3 . 3");
+    expect_tokens(4);
+    expect_token(0, "3.3", Token::Type::FLOAT);
+    expect_token(1, ".", Token::Type::OP_CAT_OLD);
+    expect_token(2, "3", Token::Type::INTEGER);
+
+    tokenize_str("4 . 4.4");
+    expect_tokens(4);
+    expect_token(0, "4", Token::Type::INTEGER);
+    expect_token(1, ".", Token::Type::OP_CAT_OLD);
+    expect_token(2, "4.4", Token::Type::FLOAT);
+
+    // TODO: '3e' from '3.3e' should be tokenized as an expression
+
+    tokenize_str("5.5e");
+    expect_tokens(5);
+    expect_token(0, "5", Token::Type::INTEGER);
+    expect_token(1, ".", Token::Type::OP_CAT_OLD);
+    expect_token(2, "5", Token::Type::INTEGER);
+    expect_token(3, "e", Token::Type::IDENTIFIER);
+
+    tokenize_str("-6.6");
+    expect_tokens(3);
+    expect_token(0, "-", Token::Type::OP_UNARY_MINUS);
+    expect_token(1, "6.6", Token::Type::FLOAT);
+}
+
+TEST_F(TokenizerTest, IntegerTokenTest)
+{
+    tokenize_str("0x");
+    expect_tokens(3);
+    expect_token(0, "0", Token::Type::INTEGER);
+    expect_token(1, "x", Token::Type::IDENTIFIER);
+
+    tokenize_str("0xag");
+    expect_tokens(3);
+    expect_token(0, "0xa", Token::Type::INTEGER);
+    expect_token(1, "g", Token::Type::IDENTIFIER);
+
+    tokenize_str("0b12");
+    expect_tokens(3);
+    expect_token(0, "0", Token::Type::INTEGER);
+    expect_token(1, "b12", Token::Type::IDENTIFIER);
 }
 
 TEST_F(TokenizerTest, Test01)
 {
     // TODO: consider cmake 'configure_file(...)' to copy from src to build
+
     tokenize_file("../../test/sample/01.vim");
 
     expect_tokens(6);
