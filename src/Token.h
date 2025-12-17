a95ee3f452af5dee19a99329f2d343d523ac210b
Tyler Carbone <tcarbone073@gmail.com>
2025-12-17 09:12:49 -0500

    [Lex] Token: use X macro pattern to generate enums and case labels

--- src/Token.h
+++ src/Token.h
@@ -1,93 +1,93 @@
 #ifndef TOKEN_H
 #define TOKEN_H
 
-#include <string>
 #include <ostream>
+#include <string>
+
+// clang-format off
+#define TOKEN_TYPE_LIST \
+        X(NONE) \
+        X(CMD_LET) \
+        X(CMD_ECHO) \
+        X(CMD_SET) \
+        X(GEN_PLUS) \
+        X(GEN_MINUS) \
+        X(GEN_QUESTION) \
+        X(GEN_NAME) \
+        X(OP_ADD) \
+        X(OP_SUB) \
+        X(OP_UNARY_PLUS) \
+        X(OP_UNARY_MINUS) \
+        X(OP_MUL) \
+        X(OP_DIV) \
+        X(OP_MODULO) \
+        X(OP_EQUAL) \
+        X(OP_NEQUAL) \
+        X(OP_GT) \
+        X(OP_GTE) \
+        X(OP_LT) \
+        X(OP_LTE) \
+        X(OP_MATCH) \
+        X(OP_NMATCH) \
+        X(OP_IS) \
+        X(OP_ISNOT) \
+        X(OP_OR) \
+        X(OP_AND) \
+        X(OP_MATCH_CASE) \
+        X(OP_IGNORE_CASE) \
+        X(OP_TERNARY_IF) \
+        X(OP_TERNARY_ELSE) \
+        X(OP_FALSEY) \
+        X(OP_LSHIFT) \
+        X(OP_RSHIFT) \
+        X(OP_CAT_OLD) \
+        X(OP_CAT_NEW) \
+        X(OP_LOGICAL_NOT) \
+        X(OP_OPTION) \
+        X(ASSIGN_ADD) \
+        X(ASSIGN_MINUS) \
+        X(ASSIGN_MUL) \
+        X(ASSIGN_DIV) \
+        X(ASSIGN_EQ) \
+        X(ASSIGN_MODULO) \
+        X(ASSIGN_CAT_OLD) \
+        X(ASSIGN_CAT_NEW) \
+        X(L_PAREN) \
+        X(R_PAREN) \
+        X(L_BRACKET) \
+        X(R_BRACKET) \
+        X(COMMA) \
+        X(IF) \
+        X(ELSEIF) \
+        X(ELSE) \
+        X(ENDIF) \
+        X(WHILE) \
+        X(ENDWHILE) \
+        X(FOR) \
+        X(IN) \
+        X(ENDFOR) \
+        X(BREAK) \
+        X(CONTINUE) \
+        X(END) \
+        X(TAB) \
+        X(NEWLINE) \
+        X(SPACE) \
+        X(OPTION) \
+        X(IDENTIFIER) \
+        X(STRING) \
+        X(INTEGER) \
+        X(FLOAT)
+
+// clang-format on
 
 class Token
 {
 public:
     enum class Type
     {
-        NONE,
-
-        CMD_LET,   // let
-        CMD_ECHO,  // echo
-        CMD_SET,   // set
-
-        GEN_PLUS,      // +
-        GEN_MINUS,     // -
-        GEN_QUESTION,  // ?
-        GEN_NAME,      // [a-zA-Z_][a-zA-Z0-9_]+
-
-        OP_ADD,           // +
-        OP_SUB,           // -
-        OP_UNARY_PLUS,    // +
-        OP_UNARY_MINUS,   // -
-        OP_MUL,           // *
-        OP_DIV,           // /
-        OP_MODULO,        // %
-        OP_EQUAL,         // ==
-        OP_NEQUAL,        // !=
-        OP_GT,            // >
-        OP_GTE,           // >=
-        OP_LT,            // <
-        OP_LTE,           // <=
-        OP_MATCH,         // =~
-        OP_NMATCH,        // !~
-        OP_IS,            // is
-        OP_ISNOT,         // isnot
-        OP_OR,            // ||
-        OP_AND,           // &&
-        OP_MATCH_CASE,    // #
-        OP_IGNORE_CASE,   // ?
-        OP_TERNARY_IF,    // ?
-        OP_TERNARY_ELSE,  // :
-        OP_FALSEY,        // ??
-        OP_LSHIFT,        // <<
-        OP_RSHIFT,        // >>
-        OP_CAT_OLD,       // .
-        OP_CAT_NEW,       // ..
-        OP_LOGICAL_NOT,   // !
-        OP_OPTION,        // &
-
-        ASSIGN_ADD,      // +=
-        ASSIGN_MINUS,    // -=
-        ASSIGN_MUL,      // *=
-        ASSIGN_DIV,      // /=
-        ASSIGN_EQ,       // =
-        ASSIGN_MODULO,   // %=
-        ASSIGN_CAT_OLD,  // .=
-        ASSIGN_CAT_NEW,  // ..=
-
-        L_PAREN,    // (
-        R_PAREN,    // )
-        L_BRACKET,  // [
-        R_BRACKET,  // ]
-        COMMA,      // ,
-
-        IF,        // if
-        ELSEIF,    // elseif
-        ELSE,      // else
-        ENDIF,     // endif
-        WHILE,     // while
-        ENDWHILE,  // endwhile
-        FOR,       // for
-        IN,        // in
-        ENDFOR,    // endfor
-        BREAK,     // break
-        CONTINUE,  // continue
-
-        END,      // End of file
-        TAB,      // 0x09
-        NEWLINE,  // 0x0a
-        SPACE,    // 0x20
-
-        OPTION,      // [a-z]+
-        IDENTIFIER,  // [a-zA-Z_][a-zA-Z0-9_]+
-        STRING,      // "..." or '...'
-        INTEGER,     // 42, 0xff, 0b1101, etc.
-        FLOAT,       // 3.14, 6.022e+23, -1.4E-5, etc.
+#define X(name) name,
+        TOKEN_TYPE_LIST
+#undef X
     };
 
     Token();
