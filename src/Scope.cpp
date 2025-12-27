972e2d8d3636facfddf3cdafeec8f947fe6a4a84
Tyler Carbone <tcarbone073@gmail.com>
2025-12-27 18:27:38 -0500

    Scope: refine Scope (augments bb0f1cf)

--- src/Scope.cpp
+++ src/Scope.cpp
@@ -5,12 +5,49 @@ Scope::Scope(Scope* apScope) :
 {
 }
 
-void Scope::insert(const Symbol& acSymbol)
+Scope::~Scope()
 {
-    m_mSymbols.insert({ acSymbol.name(), acSymbol });
+    for (auto it = m_mSymbols.cbegin(); it != m_mSymbols.cend(); it++)
+    {
+        delete it->second;
+    }
+
+    m_mSymbols.clear();
 }
 
-const Symbol& Scope::lookup(const std::string& asName) const
+Scope* Scope::parent() const
 {
+    return m_pParent;
+}
+
+void Scope::set_parent(Scope* apScope)
+{
+    m_pParent = apScope;
+}
+
+void Scope::insert(const Symbol* apSymbol)
+{
+    m_mSymbols.insert({ apSymbol->name(), apSymbol });
+}
+
+void Scope::insert(const std::string& asName, Symbol::Type aeType)
+{
+    Symbol* pSymbol = new Symbol(aeType, asName);
+    insert(pSymbol);
+}
+
+const Symbol* Scope::lookup(const std::string& asName) const
+{
+    auto it = m_mSymbols.find(asName);
+
+    if (it != m_mSymbols.end())
+    {
+        return it->second;
+    }
+    else if (m_pParent == nullptr)
+    {
+        return nullptr;
+    }
+
     return m_mSymbols.at(asName);
 }
--- src/Scope.h
+++ src/Scope.h
@@ -11,12 +11,16 @@ public:
     Scope(Scope* parent = nullptr);
     ~Scope();
 
-    void insert(const Symbol& symbol);
-    const Symbol& lookup(const std::string& name) const;
+    Scope* parent() const;
+    void set_parent(Scope* scope);
+
+    void insert(const Symbol* symbol);
+    void insert(const std::string& name, Symbol::Type type);
+    const Symbol* lookup(const std::string& name) const;
 
 private:
     Scope* m_pParent;
-    std::unordered_map<std::string, Symbol> m_mSymbols;
+    std::unordered_map<std::string, const Symbol*> m_mSymbols;
 };
 
 #endif  // SCOPE_H
