44cf15b61fbe0df2731a79f9c9e7506460a26368
Tyler Carbone <tcarbone073@gmail.com>
2025-12-27 18:58:02 -0500

    Context: add Scope

--- src/Context.cpp
+++ src/Context.cpp
@@ -1,11 +1,16 @@
 #include "Context.h"
 
-Context::Context()
+Context::Context() :
+    m_pScope { new Scope() }
 {
 }
 
 Context::~Context()
 {
+    for (Scope* pScope = pop_scope(); pScope != nullptr; pScope = pop_scope())
+    {
+        delete pScope;
+    }
 }
 
 Settings& Context::settings()
@@ -26,4 +31,22 @@ void Context::add_text(const std::string& asText)
 const Source& Context::source() const
 {
     return m_cSource;
+}
+
+Scope* Context::curr_scope()
+{
+    return m_pScope;
+}
+
+void Context::push_scope()
+{
+    m_pScope = new Scope(m_pScope);
+}
+
+Scope* Context::pop_scope()
+{
+    Scope* pOld = m_pScope;
+    pOld->set_parent(nullptr);
+    m_pScope = pOld->parent();
+    return pOld;
 }
--- src/Context.h
+++ src/Context.h
@@ -1,6 +1,7 @@
 #ifndef CONTEXT_H
 #define CONTEXT_H
 
+#include "Scope.h"
 #include "Settings.h"
 #include "Source.h"
 
@@ -16,9 +17,14 @@ public:
     void add_path(const std::string& path);
     void add_text(const std::string& text);
 
+    void push_scope();
+    Scope* curr_scope();
+    Scope* pop_scope();
+
 private:
     Settings m_cSettings;
     Source m_cSource;
+    Scope* m_pScope;
 };
 
 #endif  // CONTEXT_H
