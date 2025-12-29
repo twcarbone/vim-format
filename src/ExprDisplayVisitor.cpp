d5ba4606e3b2c805d79e0db2dbdcf2e565a80fa0
Tyler Carbone <tcarbone073@gmail.com>
2025-12-29 11:59:25 -0500

    Create src/ExprDisplayVisitor.[h,cpp]

---
+++ src/ExprDisplayVisitor.cpp
@@ -0,0 +1,37 @@
+#include "ExprDisplayVisitor.h"
+#include "ast.h"
+
+std::string ExprDisplayVisitor::display(ast::Node* apRoot)
+{
+    m_sDisplay.clear();
+
+    apRoot->accept(*this);
+
+    // Remove leading whitespace
+    return m_sDisplay.substr(1);
+}
+
+void ExprDisplayVisitor::visit(const ast::BinaryOp* apBinaryOp)
+{
+    m_sDisplay += " (" + apBinaryOp->op()->str();
+    apBinaryOp->left()->accept(*this);
+    apBinaryOp->right()->accept(*this);
+    m_sDisplay += ")";
+}
+
+void ExprDisplayVisitor::visit(const ast::Literal* apLiteral)
+{
+    m_sDisplay += " " + apLiteral->token()->str();
+}
+
+void ExprDisplayVisitor::visit(const ast::UnaryOp* apUnaryOp)
+{
+    m_sDisplay += " (" + apUnaryOp->op()->str();
+    apUnaryOp->accept(*this);
+    m_sDisplay += ")";
+}
+
+void ExprDisplayVisitor::visit(const ast::Var* apVar)
+{
+    m_sDisplay += " " + apVar->token()->str();
+}
---
+++ src/ExprDisplayVisitor.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <string>
+
+#include "ASTVisitor.h"
+
+namespace ast
+{
+class BinaryOp;
+class Literal;
+class Node;
+class UnaryOp;
+class Var;
+};
+
+class ExprDisplayVisitor : public ASTVisitor
+{
+public:
+    ExprDisplayVisitor() = default;
+    ~ExprDisplayVisitor() = default;
+
+    /**
+     *  @brief
+     *      Return the expression AST `root` in prefix notation.
+     *
+     *  @example
+     *      1 + 2 * 3               => (+ 1 (* 2 3))
+     *      -1 + 2 * -3 / 4 - 5     => (- (+ (- 1) (/ (* 2 (- 3)) 4)) 5)
+     */
+    std::string display(ast::Node* root);
+
+    virtual void visit(const ast::BinaryOp* ast) override;
+    virtual void visit(const ast::Literal* ast) override;
+    virtual void visit(const ast::UnaryOp* ast) override;
+    virtual void visit(const ast::Var* ast) override;
+
+private:
+    std::string m_sDisplay;
+};
