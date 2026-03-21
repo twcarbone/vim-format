e0104599eed2f04748b26c53e8db9a3b7f889298
Tyler Carbone <tcarbone073@gmail.com>
2026-03-21 10:30:26 -0400

    Add CmdLine implementation flag

--- scripts/cmds.py
+++ scripts/cmds.py
@@ -14,7 +14,7 @@ RE2 = ":(?:(?:\[.+?\])|(?:\{.+?\}))?(.+?(?:]|$))"
 
 
 class CmdLine:
-    __slots__ = ["_id", "_line", "_cmd", "_full", "_first"]
+    __slots__ = ["_id", "_line", "_cmd", "_full", "_first", "_impl"]
 
     def __init__(self, line_nr, line):
         match = re.match(RE1, line)
@@ -24,6 +24,7 @@ class CmdLine:
         self._first = self._full.split(" ")[0]
         self._build_id()
         self._build_cmd()
+        self._build_impl()
 
     def _build_id(self):
         tmp = zlib.crc32(self._full.encode("utf-8"))
@@ -37,6 +38,20 @@ class CmdLine:
         tmp = tmp.replace("]", "")
         tmp = ":" + tmp
         self._cmd = tmp
+
+    def _build_impl(self):
+        for path in pathlib.Path("src").glob("*.cpp"):
+            with open(path, "r") as f:
+                for i, line in enumerate(f.readlines()):
+                    match = re.match(f"^// {self._id}$", line)
+                    if match is None:
+                        self._impl = ""
+                    else:
+                        self._impl = str(path) + ":" + str(i + 1)
+                        return
+
+    def has_impl(self):
+        return self._impl != ""
 
 
 def main():
@@ -71,6 +86,8 @@ def main():
                 cmdlines.append(cmdline)
 
         for line in cmdlines:
+            print("  ", end="")
+            print(f"{'Y' if line.has_impl() else ' '}{DELIM}", end="")
             print(f"{line._id}{DELIM}", end="")
             print(f"{line._line:<{ln_len}}{DELIM}", end="")
             print(f"{line._cmd:<{cmd_len}}{DELIM}", end="")
